642b3185bfd271f9b1a95334dadc0aad8e5588e1
Finci35 <2349fatih@gmail.com>
2022-12-10 01:13:09 +0100

    variable

--- variables/variable/src/Main.java
+++ variables/variable/src/Main.java
@@ -2,17 +2,18 @@ public class Main {
     public static void main(String[] args) {
         int ogrenciSayisi = 11; // Burada "ogrenciSayisi" değişkenini tanımladık.
         // Değişken tanımlarken "camel casing" kullanılır.
-        // Değişkendeki ilk keleminin ilk harfi küçük, diğer kelimelerin ilk harfi büyük anlamına gelir.
-        // REUSABILITY : Tekrar tekrar kullanılabilirlik demektir. Bir kere yaz, onu heryerde kullan.
+        // Değişkendeki ilk keleminin ilk harfi küçük, diğer kelimelerin ilk harfi büyük
+        // anlamına gelir.
+        // REUSABILITY : Tekrar tekrar kullanılabilirlik demektir. Bir kere yaz, onu
+        // heryerde kullan.
 
-        String mesaj = "Öğrenci Sayısı :"; // Burada "mesaj" isminde bir değişken tanımladık.
+        String mesaj = "Ögrenci Sayısı :"; // Burada "mesaj" isminde bir değişken tanımladık.
 
-        System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+        System.out.println("Ögrenci sayım : " + ogrenciSayisi);
 
         System.out.println(mesaj + ogrenciSayisi);
 
-
-        System.out.println("Öğrenci sayım : 9");
-        System.out.println("Öğrenci sayım : 9");
+        System.out.println("Ögrenci sayım : 9");
+        System.out.println("Ögrenci sayım : 9");
     }
 }
