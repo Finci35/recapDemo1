8407d7287a8569697578994726539387d3c7cb0a
Finci35 <2349fatih@gmail.com>
2023-05-10 17:18:31 +0200

    10.05-17.15

---
+++ LF_11a_V2/src/Zahlendreieck.java
@@ -0,0 +1,22 @@
+
+public class Zahlendreieck {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		String schleife = "0 ";
+		
+		while ( i < 10) {
+			schleife += i + " ";  
+			System.out.println(schleife);
+			i++;
+		}
+		
+		for(int z=0; z<10; z++) {
+			for(int s=0; s-1<z; s++);
+			System.out.print(s);
+		}
+
+	}
+
+}
