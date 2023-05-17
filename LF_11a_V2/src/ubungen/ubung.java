0e277ba2927ab4f6116c288a600b942ea4d5808a
Finci35 <2349fatih@gmail.com>
2023-05-18 00:35:09 +0200

    18.05-00.35

--- LF_11a_V2/src/ubungen/ubung.java
+++ LF_11a_V2/src/ubungen/ubung.java
@@ -24,7 +24,45 @@ public class ubung {
 		System. out. println() ; 
 		
 		
+		int []array = {1, 2, 3, 4, 5};
+
+		for (int i: array) {
+			if(i < 2) { 
+				continue; 
+			}
+			System.out.print(i); 
+			if (i == 3){
+				continue; 
+			}
+			
+		}
+		
+		System. out. println() ; 
+		
+		int iArray[] = {65, 68, 69};
+		 
+		iArray[2] = iArray[0]; 
+		iArray[0] = iArray[1]; 
+		iArray[1] = iArray[2]; 
+
+		for (int element : iArray) { 
+			System. out. print(element + " ");
+		}
+		
+		System. out. println() ; 
+		
+		int num[] = { 10, 15, 2, 17}; 
+		int sum = 0; 
+
+		for (int number : num) {
+			if (number % 2 != 0)
+					continue; 
+			sum += number;
+		}
+		System.out.println(sum);
 		
 	}
-
+	
+	
 }
+
